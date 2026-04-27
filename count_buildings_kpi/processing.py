1a2dd92ddc228a733d8d3041f18fbebf26e4db0b
Anastasiia Pohonets <29karasik@gmail.com>
2026-04-27 20:37:43 +0300

    update processing

--- count_buildings_kpi/processing.py
+++ count_buildings_kpi/processing.py
@@ -26,24 +26,9 @@ def filter_image(img_cv):
     return img_cv_filtered
 
 
-def gabor_filter(gray):
-    kernel = cv2.getGaborKernel(
-        (21, 21),
-        8.0,
-        np.pi / 4,
-        10.0,
-        0.5,
-        0,
-        ktype=cv2.CV_32F
-    )
-    filtered = cv2.filter2D(gray, cv2.CV_8UC3, kernel)
-    return filtered
-
-
 def image_processing(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
-    # gabor = gabor_filter(gray) # Gabor фільтр
     edged = cv2.Canny(gray, 100, 200)
     kernel_close = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     kernel_open = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
